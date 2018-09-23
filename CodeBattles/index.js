d7d3ba7bb1da3fdb15977da557769618ea073f9f
Zack <zack@mateja.com>
2018-09-23 14:22:09 -0400

    add secondary page to redirect user after code submission

--- CodeBattles/index.js
+++ CodeBattles/index.js
@@ -1,5 +1,5 @@
 var myCodeMirror = CodeMirror(document.body, {
-    value: "public class CodeBattles",
+    value: "public class CodeBattles{\n\t public static void main (String[] args){\n\n\t}\n}",
     mode:  "text/x-java",
     lineNumbers: true,
     matchBrackets: true,
